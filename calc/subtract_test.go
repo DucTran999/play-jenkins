7105bbf65441af8db189098646052588defff41e
ductran999 <ducnta.99.ptit@gmail.com>
2024-11-01 14:48:43 +0700

    rm not clean code

--- calc/subtract_test.go
+++ calc/subtract_test.go
@@ -1,23 +1,16 @@
 package calc_test
 
-import (
-	"testing"
+// func TestSub(t *testing.T) {
+// 	tests := []struct {
+// 		input    []int
+// 		expected int
+// 	}{
+// 		{input: []int{}, expected: 0},
+// 		{input: []int{1, 2}, expected: -1},
+// 	}
 
-	"github.com/DucTran999/play-jenkins/calc"
-	"github.com/stretchr/testify/assert"
-)
-
-func TestSub(t *testing.T) {
-	tests := []struct {
-		input    []int
-		expected int
-	}{
-		{input: []int{}, expected: 0},
-		{input: []int{1, 2}, expected: -1},
-	}
-
-	for _, tt := range tests {
-		result := calc.Sub(tt.input...)
-		assert.Equal(t, tt.expected, result)
-	}
-}
+// 	for _, tt := range tests {
+// 		result := calc.Sub(tt.input...)
+// 		assert.Equal(t, tt.expected, result)
+// 	}
+// }
